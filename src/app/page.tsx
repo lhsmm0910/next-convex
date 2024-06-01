a87da60f4ec8944a98eaff595d1626281115ed95
luces <lubo@cqop.com>
2024-06-01 13:32:10 +0800

    2

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,7 @@ import { api } from "../../convex/_generated/api";
 
 export default function Home() {
   const tasks = useQuery(api.tasks.get);
+  console.log(2)
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       {tasks?.map(({ _id, text }) => <div key={_id}>{text}</div>)}
